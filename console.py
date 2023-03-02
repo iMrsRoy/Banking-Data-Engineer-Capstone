359838f71f22073463b77ef02522329ff0338299
Rajarshi Roy ♠ <10099786+iMrsRoy@users.noreply.github.com>
2023-03-01 20:24:06 -0500

    pushing 5 viz

--- console.py
+++ console.py
@@ -1,44 +1,100 @@
-import pandas as pd
+import mysql.connector
 
-def main():
-    # read CSV file into a Pandas DataFrame
-    df = pd.read_csv("path/to/input/file.csv")
-    
-    # display options to the user
-    print("Welcome to the data viewer program!")
-    print(f"Loaded data from file: path/to/input/file.csv")
-    print("Select an option:")
-    print("1. View data")
-    print("2. Filter data")
-    print("3. Exit")
-    
+# define MySQL connection parameters
+mysql_hostname = "localhost"
+mysql_port = 3306
+mysql_database = "creditcard_capstone"
+mysql_username = "root"
+mysql_password = "ShaShi3493*"
+# connect to MySQL database
+cnx = mysql.connector.connect(user=mysql_username, password=mysql_password,
+                              host=mysql_hostname, database=mysql_database)
+
+query = "(SELECT * FROM cdw_sapp_branch bc \
+      JOIN cdw_sapp_credit_card cc ON bc.BRANCH_CODE = cc.BRANCH_CODE \
+        JOIN cdw_sapp_customer as cust ON cc.CUST_SSN = cust.SSN)"
+
+def execute_query(query):
+    return mysql(query).toPandas()
+
+# define a function to execute SQL queries and return results
+def execute_query(query):
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
+# define a function to validate user input
+def validate_input(prompt, expected_type):
     while True:
-        # prompt the user for input
-        choice = input("Enter your choice (1-3): ")
-        
-        # option 1: view data
-        if choice == "1":
-            print(df)
-        
-        # option 2: filter data
-        elif choice == "2":
-            # prompt the user for a filter condition
-            column_name = input("Enter column name to filter by: ")
-            operator = input("Enter operator (==, >, <, >=, <=, !=): ")
-            value = input("Enter value to compare to: ")
-            
-            # filter the data and display the result
-            filtered_df = df.query(f"{column_name} {operator} {value}")
-            print(filtered_df)
-        
-        # option 3: exit
-        elif choice == "3":
-            print("Exiting program...")
-            break
-        
-        # invalid input
+        user_input = input(prompt)
+        try:
+            validated_input = expected_type(user_input)
+            return validated_input
+        except ValueError:
+            print(f"Invalid input. Please enter a {expected_type.__name__}.")
+
+# define a loop for user interaction
+while True:
+    print("Select an option:")
+    print("1. View all records")
+    print("2. View a specific record by ID")
+    print("3. Add a new record")
+    print("4. Update an existing record")
+    print("5. Delete a record")
+    print("6. Exit")
+
+    # validate user input
+    choice = validate_input("Enter your choice: ", int)
+    if choice == 1:
+        # view all records
+        result = execute_query("SELECT * FROM my_table")
+        for row in result:
+            print(row)
+    elif choice == 2:
+        # view a specific record by ID
+        id = validate_input("Enter the ID of the record you want to view: ", int)
+        result = execute_query(f"SELECT * FROM my_table WHERE id = {id}")
+        if result:
+            print(result[0])
         else:
-            print("Invalid choice. Please enter a number between 1 and 3.")
-    
-if __name__ == "__main__":
-    main()
+            print("Record not found.")
+    elif choice == 3:
+        # add a new record
+        name = input("Enter the name: ")
+        age = validate_input("Enter the age: ", int)
+        execute_query(f"INSERT INTO my_table (name, age) VALUES ('{name}', {age})")
+        cnx.commit()
+        print("Record added.")
+    elif choice == 4:
+        # update an existing record
+        id = validate_input("Enter the ID of the record you want to update: ", int)
+        result = execute_query(f"SELECT * FROM my_table WHERE id = {id}")
+        if result:
+            print(f"Current record: {result[0]}")
+            name = input("Enter the new name (leave blank to keep current value): ")
+            age = input("Enter the new age (leave blank to keep current value): ")
+            if name == "":
+                name = result[0][1]
+            if age == "":
+                age = result[0][2]
+            execute_query(f"UPDATE my_table SET name = '{name}', age = {age} WHERE id = {id}")
+            cnx.commit()
+            print("Record updated.")
+        else:
+            print("Record not found.")
+    elif choice == 5:
+        # delete a record
+        id = validate_input("Enter the ID of the record you want to delete: ", int)
+        execute_query(f"DELETE FROM my_table WHERE id = {id}")
+        cnx.commit()
+        print("Record deleted.")
+    elif choice == 6:
+        # exit
+        break
+    else:
+        print("Invalid choice.")
+        
+# close MySQL connection
+
--- pyconsole.py
+++ pyconsole.py
@@ -6,13 +6,9 @@ import pyinputplus as pyin
 import mysql.connector as mysql
 import warnings
 
-# suppress warnings
-warnings.filterwarnings('ignore')
-
-# create a SparkSession
-spark = SparkSession\
-    .builder\
-    .appName("PYCONSOLE")\
+# create SparkSession
+spark = SparkSession.builder \
+    .appName("Bank Transactions") \
     .config("spark.jars","/Users/roy/Downloads/mysql-connector-j-8.0.32/mysql-connector-j-8.0.32.jar")\
     .getOrCreate()
 
@@ -23,29 +19,16 @@ mysql_database = "creditcard_capstone"
 mysql_username = "root"
 mysql_password = "ShaShi3493*"
 
-# define a function to execute SQL queries and return a PySpark DataFrame
+# suppress pyspark warnings
+warnings.filterwarnings("ignore", category=UserWarning, message=".*Please use.*")
 
 query = "(SELECT * FROM cdw_sapp_branch bc \
       JOIN cdw_sapp_credit_card cc ON bc.BRANCH_CODE = cc.BRANCH_CODE \
         JOIN cdw_sapp_customer as cust ON cc.CUST_SSN = cust.SSN)"
 
+# define a function to execute SQL queries and return results
 def execute_query(query):
-    df = spark.read.format("jdbc")\
-        .option("url", f"jdbc:mysql://{mysql_hostname}:{mysql_port}/{mysql_database}")\
-            .option("driver", "/Users/roy/Downloads/mysql-connector-j-8.0.32/mysql-connector-j-8.0.32.jar")\
-                .option("dbtable", f"({query})")\
-                    .option("user", mysql_username)\
-                        .option("password", mysql_password)\
-                            .load()
-    return df
-def execute_query(query):
-    try:
-        result = spark.sql(query).toPandas()
-        return result
-    except Exception as e:
-        print(f"Error executing query: {e}")
-        return None
-
+    return spark.sql(query).toPandas()
 
 # define a function to validate user input
 def validate_input(prompt, expected_type):
@@ -60,43 +43,37 @@ def validate_input(prompt, expected_type):
 # define a loop for user interaction
 while True:
     print("Select an option:")
-    print("1. Display transactions by zip code and month/year")
-    print("2. Display number and total value of transactions by type")
-    print("3. Display total number and value of transactions by state")
+    print("1. Display transactions by zip code and date")
+    print("2. Display transaction count and total value by type")
+    print("3. Display transaction count and total value by branch state")
     print("4. Exit")
 
     # validate user input
     choice = validate_input("Enter your choice: ", int)
     if choice == 1:
-        #1)    Used to display the transactions made by customers living in a given zip code for a given month and year. Order by day in descending order.
-       
+        # display transactions by zip code and date
         zip_code = validate_input("Enter the zip code: ", int)
-        year = validate_input("Enter the year (YYYY): ", int)
-        month = validate_input("Enter the month (MM): ", int)
-        result = execute_query(f"SELECT * FROM CDW_SAPP_CUSTOMER, CDW_SAPP_BRANCH WHERE CUST_ZIP = {zip_code} AND YEAR(date) = {year} AND MONTH(date) = {month} ORDER BY date DESC")
-        if result:
-            pd_result = result.toPandas()
-            print(pd_result)
+        year = validate_input("Enter the year: ", int)
+        month = validate_input("Enter the month: ", int)
+        result = execute_query(f"SELECT * FROM transactions WHERE zip_code = {zip_code} AND year = {year} AND month = {month} ORDER BY day DESC")
+        if not result.empty:
+            print(result)
         else:
             print("No transactions found.")
     elif choice == 2:
-        #2)    Used to display the number and total values of transactions for a given type.
-      
+        # display transaction count and total value by type
         transaction_type = validate_input("Enter the transaction type: ", str)
-        result = execute_query(f"SELECT TRANSACTION_VALUE, TRANSACTION_TYPE FROM '{query}' where TRANSACTION_TYPE  = '{transaction_type}'")
-        if result:
-            pd_result = result.toPandas()
-            print(pd_result)
+        result = execute_query(f"SELECT COUNT(*) AS transaction_count, SUM(amount) AS total_value FROM transactions WHERE type = '{transaction_type}'")
+        if not result.empty:
+            print(result)
         else:
             print("No transactions found.")
     elif choice == 3:
-        #3)    Used to display the total number and total values of transactions for branches in a given state.
-    
+        # display transaction count and total value by branch state
         state = validate_input("Enter the state: ", str)
-        result = execute_query(f"SELECT bc.state, COUNT(*) as count, SUM(TRANSACTION_VALUE) as total_value FROM cdw_sapp_branch bc JOIN cdw_sapp_credit_card cc ON bc.BRANCH_CODE = cc.BRANCH_CODE WHERE BRANCH_STATE = '{state}'")
-        if result:
-            pd_result = result.toPandas()
-            print(pd_result)
+        result = execute_query(f"SELECT COUNT(*) AS transaction_count, SUM(amount) AS total_value FROM transactions JOIN branches ON transactions.branch_id = branches.id WHERE branches.state = '{state}'")
+        if not result.empty:
+            print(result)
         else:
             print("No transactions found.")
     elif choice == 4:
@@ -104,5 +81,6 @@ while True:
         break
     else:
         print("Invalid choice.")
-
         
+# stop SparkSession
+spark.stop()
